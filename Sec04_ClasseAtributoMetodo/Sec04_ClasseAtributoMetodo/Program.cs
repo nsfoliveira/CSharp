fdaafc53a8b16a1f7116b480b930c19cc4b7acce
Natália Oliveira <nsfoliveira90@gmail.com>
2022-08-17 18:02:38 -0300

    update
    aprendendo POO

---
+++ Sec04_ClasseAtributoMetodo/Sec04_ClasseAtributoMetodo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sec04_ClasseAtributoMetodo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Sec04_ClasseAtributoMetodo/Sec04_ClasseAtributoMetodo/Triangulo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sec04_ClasseAtributoMetodo
+{
+    internal class Triangulo
+    {
+    }
+}
