54740ceba67f4e2580623a94244e77e8011616ed
Natália Oliveira <nsfoliveira90@gmail.com>
2022-08-22 19:12:32 -0300

    update

---
+++ ExercicioB_NomeSalario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioB_NomeSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
