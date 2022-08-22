45e88047a4fbc3042ffd5f09fd80fff4f3e17b5d
Natália Oliveira <nsfoliveira90@gmail.com>
2022-08-22 19:29:32 -0300

    Exercicio01_A
    Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais
    velha.

---
+++ Exercicio01_CriandoClasse/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioA_NomeIdade
+{
+    class PessoaNomeIdade
+    {
+        public string _nome;
+        public int _idade;
+    }
+}
--- Exercicio01_CriandoClasse/Program.cs
+++ Exercicio01_CriandoClasse/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Exercicio01_CriandoClasse
-{
-    internal class Program
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioA_NomeIdade {
+    class AplicacaoNomeIdade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //instanciando e declarando
+            PessoaNomeIdade p1 = new PessoaNomeIdade();
+            PessoaNomeIdade p2 = new PessoaNomeIdade();
+
+            //entrada dos dados
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            p1._nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1._idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            p2._nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2._idade = int.Parse(Console.ReadLine());
+
+            //verificando a idade da pessoa mais velha
+            if (p1._idade > p2._idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1._nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2._nome);
+            }
         }
     }
 }
