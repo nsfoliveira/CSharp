7643ca10934a221689428d45fa11726ab16e0fa3
Natália Oliveira <nsfoliveira90@gmail.com>
2022-08-22 21:41:08 -0300

    ATUALIZA

---
+++ ExercicioGuiado_Estoque/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace ExercicioGuiado_Estoque
+{
+    internal class Produto
+    {
+        //atributos da classe
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //operação: total em estoque
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        //utilizando void para método que atualiza quantidade de itens que entraram
+        public void AtualizaProdutos(int quantidade)
+        {
+            Quantidade += quantidade;    
+        }
+
+        //utilizando void para método que atualiza quantidade de itens que sairam
+        public void RemoveProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        //fazendo sobreposição
+        //override indica que essa operação veio de outra classe
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercicioGuiado_Estoque/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ExercicioGuiado_Estoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarando e instanciando
+            Produto p = new Produto();
+
+            //entrada dos dados
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+
+            //saida dados do produto
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            //novas quantidades adicionadas ao estoque
+            Console.Write("Digite a quantidade de produtos a ser adicionada:  ");
+            int qtd = int.Parse(Console.ReadLine());
+            p.AtualizaProdutos(qtd);
+            Console.WriteLine("Dados do atualizados: " + p);
+
+            //quantidades removidas ao estoque
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos a ser removida:  ");
+            qtd = int.Parse(Console.ReadLine());
+            p.RemoveProduto(qtd);
+            Console.WriteLine("Dados do atualizados: " + p);
+
+
+        }
+    }
+}
