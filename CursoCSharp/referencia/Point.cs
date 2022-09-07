765bd39cd9065521f3177ba96c7f35f9c341ea0c
Natália Oliveira <nsfoliveira90@gmail.com>
2022-09-06 21:44:21 -0300

    oraganizando repositorio

---
+++ CursoCSharp/referencia/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Point
+{
+    struct PointStruct
+    {
+        public int X;
+        public int Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
---
+++ CursoCSharp/referencia/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PointStruct p;
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p); //mostrará na tela os valores declarados
+
+            p = new PointStruct(); 
+            Console.WriteLine(p);//ele recria as caixinhas vazias 
+
+
+        }
+    }
+}
